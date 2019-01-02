cf53efa7e6286a580ac84bb555b81d79af99fa69
yakir12 <12.yakir@gmail.com>
2019-01-02 14:14:19 +0100

    removed units

--- src/RayTraceEllipsoids.jl
+++ src/RayTraceEllipsoids.jl
@@ -1,88 +1,86 @@
 module RayTraceEllipsoids
 
-using CoordinateTransformations, StaticArrays, LinearAlgebra
+using CoordinateTransformations
+using StaticArrays: SVector, SDiagonal
+using LinearAlgebra: normalize, ⋅, norm
 
-export V3c, Ray, Ellipsoid, Interface, OpticUnit, raytrace!, Medium, NonRetina, Retina, Interface, Glued, Active, Near, Far, Light, Cylinder, cspheroid
+export V3, Ray, Ellipsoid, Interface, OpticUnit, raytrace!, Medium, NonRetina, Retina, Interface, Glued, Active, Light, Cylinder, cspheroid
 
 const ENERGYTHRESHOLD = 0.01
 
 """
-    V3c = SVector{3,Float64}
+    V3 = SVector{3,Float64}
 
-Points and directions are just a point in 3D space best described as a static V3ctor, `SVector`. `V3c` is an alias for that.
+Points and directions are just a point in 3D space best described as a static `SVector`. `V3` is an alias for that.
 """
-const V3c = SVector{3}
+const V3 = SVector{3, Float64}
+
+abstract type AbstractRay end
 
 """
-    Ray(orig::V3c, dir::V3c)
+    Ray(orig::V3, dir::V3, int::Float64)
 
-The main Ray type with a ray origin, `orig`, and direction, `dir`. The ray's direction gets normalized.
+The main Ray type with a ray origin, `orig`, a direction, `dir`, and an intensity `int`. The ray's direction gets normalized.
 """
-mutable struct Ray{T <: AbstractFloat}
-    orig::V3c{T}
-    dir::V3c{T}
-    int::T
-    Ray(o::V3c{T}, d::V3c{T}) where {T <: AbstractFloat} = new{T}(o, normalize(d), one(T))
+mutable struct Ray <: AbstractRay
+    orig::V3
+    dir::V3
+    int::Float64
+    Ray(o::V3, d::V3) = new(o, normalize(d), 1.0)
 end
 
 """
-    Ellipsoid(c::V3c, r::V3c, dir::V3c, open::Float64)
+    EmptyRay(orig::V3, dir::V3, int::Float64)
+
+A Ray type for plotting. Same as `Ray` but doesn't get registered in retinas. Mainly used for plotting.
+"""
+mutable struct EmptyRay <: AbstractRay
+    orig::V3
+    dir::V3
+    int::Float64
+    EmptyRay(o::V3, d::V3) = new(o, normalize(d), 1.0)
+end
+
+"""
+    Ellipsoid(c::V3, r::V3, dir::V3, open::Float64)
 
     An ellipsoid with a center, `c`, and radii, `r`, as well as a direction (gets automatically normalized), `dir`, and an opening angle, `open`, creating a dome (or window), that the ellipsoid is defined in. Note that `open` is the angle between the dome's edge and the direction of the dome (so actually half the opening angle) and is defined in **some angular units** (using UnitfulAngles, for example: u"°").
 
 `Ellipsoid` has 6 additional fields all relating to various spatial transformations that convert the ellipsoid to a unit-sphere and back again. These are all `CoordinateTransformations`.
-
-# Examples
-For an ellipsoid upwards-pointing hemisphere with a center at (1,2,3), and radii (4,5,6):
-```jldoctest
-julia> Ellipsoid(V3c(1,2,3), V3c(4,5,6), V3c(0,0,1), 0.0)
-Rayden.Ellipsoid([1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [0.0, 0.0, 1.0], 0.0, Translation(-1.0, -2.0, -3.0), LinearMap([0.25 0.0 0.0; 0.0 0.2 0.0; 0.0 0.0 0.166667]), AffineMap([0.25 0.0 0.0; 0.0 0.2 0.0; 0.0 0.0 0.166667], [-0.25, -0.4, -0.5]), Translation(1.0, 2.0, 3.0), LinearMap([4.0 0.0 0.0; 0.0 5.0 0.0; 0.0 0.0 6.0]), AffineMap([4.0 0.0 0.0; 0.0 5.0 0.0; 0.0 0.0 6.0], [1.0, 2.0, 3.0]))
 ```
 """
-struct Ellipsoid{T <: AbstractFloat}
-    c::V3c{T}
-    r::V3c{T}
-    h::T # the point on the vertical axis from which the dome starts
-    name::String
-    # all of the following are transformations
-    hᵀ::T # the point on the vertical axis from which the dome starts
-    center::Translation{V3c{T}} # translate the ellipsoid to zero
-    scale::LinearMap{SDiagonal{3,T}}
-    center_scale::AffineMap{SDiagonal{3,T},V3c{T}} # translate and scale to a unit-sphere
-    # uncenter::Translation{V3c{T}}
-    # unscale::LinearMap{SDiagonal{3,T}}
-    uncenter_unscale::AffineMap{SDiagonal{3,T},V3c{T}}
-    function Ellipsoid(c::V3c{T}, r::V3c{T}, h::T, name::String) where {T <: AbstractFloat}
+
+struct Ellipsoid
+    positive::Bool # is the (normalized) dome in the positive end of the z-axis?
+    center::Translation{V3} # translate the ellipsoid to zero
+    scale::LinearMap{SDiagonal{3,Float64}} # scale to a unit-sphere
+    center_scale::AffineMap{SDiagonal{3,Float64},V3} # translate and scale to a unit-sphere
+    uncenter_unscale::AffineMap{SDiagonal{3,Float64},V3}
+
+    function Ellipsoid(c::V3, r::V3, positive::Bool)
         uncenter = Translation(c)
         unscale = LinearMap(SDiagonal(r))
         center = inv(uncenter)
         scale = inv(unscale)
         center_scale = scale∘center
         uncenter_unscale = inv(center_scale)
-        hᵀ = h/r[3]
-        new{T}(c, r, h, name, hᵀ, center, scale, center_scale, uncenter_unscale)
+        new(positive, center, scale, center_scale, uncenter_unscale)
     end
+
 end
 
-cspheroid(cz, rxy, rz, h, name) = Ellipsoid(V3c(zero(cz), zero(cz), cz), V3c(rxy, rxy, rz), h, name)
+cspheroid(cz::Real, rxy::Real, rz::Real, h::Bool) = Ellipsoid(V3(0.0, 0.0, cz), V3(rxy, rxy, rz), h)
 
-function isgoodz(oz, l, dz, hᵀ)
-    z = oz + l*dz
-    if hᵀ > 0
-        z ≥ hᵀ
-    else
-        z ≤ hᵀ
-    end
-end
+isgoodz(oz, l, dz, positive::Bool) = oz > -l*dz ? positive : ~positive
 
 struct DeadRay <: Exception end
 
 """
-    distance(orig::V3c, dir::V3c)
+    distance(orig::V3, dir::V3)
 
 Return the two distances between a point with origin, `orig`, and direction, `dir`, and the two (potentially identical) intersection points with a unit-sphere. 
 """
-function distance(orig::V3c{T}, dir::V3c{T}, hᵀ::T) where {T <: AbstractFloat}
+function distance(orig::V3, dir::V3, positive::Bool)
     b = -orig⋅dir
     disc = b^2 - orig⋅orig + 1
     if disc ≥ 0
@@ -91,32 +89,34 @@ function distance(orig::V3c{T}, dir::V3c{T}, hᵀ::T) where {T <: AbstractFloat}
         if t2 ≥ 0
             t1 = b - d
             if t1 > 0 
-                isgoodz(orig[3], t1, dir[3], hᵀ) && return t1
-                isgoodz(orig[3], t2, dir[3], hᵀ) && return t2
+                isgoodz(orig[3], t1, dir[3], positive) && return t1
+                isgoodz(orig[3], t2, dir[3], positive) && return t2
             else
-                isgoodz(orig[3], t2, dir[3], hᵀ) && return t2
+                isgoodz(orig[3], t2, dir[3], positive) && return t2
             end
         end
     end
     throw(DeadRay())
 end
 
-function distance(r::Ray{T}, s::Ellipsoid{T}) where {T <: AbstractFloat}
+function distance(r::AbstractRay, s::Ellipsoid)
     orig = s.center_scale(r.orig) # transform the ray's origin according to the ellipsoid
     dir = normalize(s.scale(r.dir)) # scale the ray's direction as well
-    l = distance(orig, dir, s.hᵀ)
+    l = distance(orig, dir, s.positive)
     o = orig + l*dir
     p = s.uncenter_unscale(o)
     norm(p - r.orig)
 end
 
-struct Cylinder{T <: AbstractFloat}
-    scale::LinearMap{SDiagonal{3,T}}
-    center_scale::AffineMap{SDiagonal{3,T},V3c{T}}
-    uncenter_unscale::AffineMap{SDiagonal{3,T},V3c{T}}
-    function Cylinder(rxy::T, z1::T, z2::T) where {T <: AbstractFloat}
+struct Cylinder
+    scale::LinearMap{SDiagonal{3,Float64}}
+    center_scale::AffineMap{SDiagonal{3,Float64},V3}
+    uncenter_unscale::AffineMap{SDiagonal{3,Float64},V3}
+
+
+    function Cylinder(rxy::Real, z1::Real, z2::Real)
         cz = (z1 + z2)/2
-        uncenter = Translation(V3c(0, 0, cz))
+        uncenter = Translation(V3(0.0, 0.0, cz))
         @assert z1 < z2 "cylinder z1 seems bigger than z2"
         rz = (z2 - z1)/2
         unscale = LinearMap(SDiagonal(rxy, rxy, rz))
@@ -124,18 +124,19 @@ struct Cylinder{T <: AbstractFloat}
         scale = inv(unscale)
         center_scale = scale∘center
         uncenter_unscale = inv(center_scale)
-        new{T}(scale, center_scale, uncenter_unscale)
+        new(scale, center_scale, uncenter_unscale)
     end
+
 end
 
 
-function _getl(orig, l, dir, ucs, rorig)
+function _getl(orig::V3, l::Float64, dir::V3, ucs, rorig::V3)
     pᵗ = orig + l*dir
     p2 = ucs(pᵗ)
     norm(p2 - rorig)
 end
 
-function incylinder(r::Ray{T}, s::Cylinder{T}, L::T) where {T <: AbstractFloat}
+function incylinder(r::Ray, s::Cylinder, L::Float64)
     orig = s.center_scale(r.orig)
     dir = normalize(s.scale(r.dir))
     a = dir[1]^2 + dir[2]^2
@@ -143,21 +144,21 @@ function incylinder(r::Ray{T}, s::Cylinder{T}, L::T) where {T <: AbstractFloat}
         if orig[1]^2 + orig[2]^2 < 1
             return L
         else
-            return zero(T)
+            return 0.0
         end
     end
     b = 2orig[1]*dir[1] + 2orig[2]*dir[2]
     c = orig[1]^2 + orig[2]^2 - 1
     Δ = b^2 - 4a*c
     if Δ ≤ 0
-        return zero(T)
+        return 0.0
     end
     sqrtΔ = sqrt(Δ)
     l1 = (-b - sqrtΔ)/2a
     l2 = (-b + sqrtΔ)/2a
     l1, l2 = l1 < l2 ? (l1, l2) : (l2, l1)
     if l2 < 0
-        return zero(T)
+        return 0.0
     end
     if l1 < 0
         l = _getl(orig, l2, dir, s.uncenter_unscale, r.orig)
@@ -169,7 +170,7 @@ function incylinder(r::Ray{T}, s::Cylinder{T}, L::T) where {T <: AbstractFloat}
     else
         l1 = _getl(orig, l1, dir, s.uncenter_unscale, r.orig)
         if l1 > L
-            return zero(T)
+            return 0.0
         end
         l2 = _getl(orig, l2, dir, s.uncenter_unscale, r.orig)
         if l2 > L
@@ -180,38 +181,38 @@ function incylinder(r::Ray{T}, s::Cylinder{T}, L::T) where {T <: AbstractFloat}
     end
 end
 
-abstract type Medium{T <: AbstractFloat} end
+abstract type Medium end
 
-struct NonRetina{T <: AbstractFloat} <: Medium{T}
-    absorption_coefficient::T
-    name::String
+struct NonRetina <: Medium
+    absorption_coefficient::Float64
 end
 
-struct Retina{T <: AbstractFloat} <: Medium{T}
-    cylinder::Cylinder{T}
-    absorption_coefficient::T
-    signal::Base.RefValue{T}
-    n::Base.RefValue{Int}
-    name::String
-    function Retina(cylinder::Cylinder{T}, absorption_coefficient::T, name::String) where {T <: AbstractFloat}
-        new{T}(cylinder, absorption_coefficient, Ref(zero(T)), Ref(0), name)
+struct Retina <: Medium
+    cylinder::Cylinder
+    absorption_coefficient::Float64
+    signal::Base.RefValue{Float64}
+    # n::Base.RefValue{Int}
+    function Retina(cylinder::Cylinder, absorption_coefficient::Float64)
+        new(cylinder, absorption_coefficient, Ref(0.0))#, Ref(0))
     end
 end
 
-register!(r, l, m::NonRetina) = nothing
+register!(r::AbstractRay, l::Float64, m::NonRetina) = nothing
 
-absorption(l, absorption_coefficient) = exp(-l*absorption_coefficient)
+absorption(l::Float64, absorption_coefficient::Float64) = exp(-l*absorption_coefficient)
 
-function register!(r, l, m::Retina)
+function register!(r::Ray, l::Float64, m::Retina)
     l = incylinder(r, m.cylinder, l)
     if !iszero(l)
         m.signal[] += r.int*(1 - absorption(l, m.absorption_coefficient))
-        m.n[] += 1
+        # m.n[] += 1
     end
     nothing
 end
 
-function absorbmove!(r, l, m)
+register!(r::EmptyRay, l::Float64, m::Retina) = nothing
+
+function absorbmove!(r::AbstractRay, l::Float64, m::Medium)
     register!(r, l, m)
     r.int *= absorption(l, m.absorption_coefficient)
     r.int < ENERGYTHRESHOLD && throw(DeadRay())
@@ -221,14 +222,14 @@ end
 
 
 #=struct Retina{T <: AbstractFloat} <: Medium{T}
-    uncenter_unscale::AffineMap{SDiagonal{3,T},V3c{T}}
-    center_scale::AffineMap{SDiagonal{3,T},V3c{T}} # translate and scale to a unit-sphere
+    uncenter_unscale::AffineMap{SDiagonal{3,T},V3{T}}
+    center_scale::AffineMap{SDiagonal{3,T},V3{T}} # translate and scale to a unit-sphere
     σ::WeightedVariance{T}
     step::T
     absorbed::T
     CCD::OffsetArray{T,2,Array{T,2}}
     n::Base.RefValue{Int64}
-    function Retina(uncenter_unscale::AffineMap{SDiagonal{3,T},V3c{T}}, step::T, absorption_coefficient::T) where {T <: AbstractFloat}
+    function Retina(uncenter_unscale::AffineMap{SDiagonal{3,T},V3{T}}, step::T, absorption_coefficient::T) where {T <: AbstractFloat}
         center_scale = inv(uncenter_unscale)
         σ = WeightedVariance(T)
         absorbed = 1 - absorption(step, absorption_coefficient)
@@ -266,9 +267,9 @@ function absorb!(r, l, m::Retina)
     m.n[] += 1
 end=#
 
-abstract type Interface{T <: AbstractFloat} end
+abstract type Interface end
 
-struct Glued{T <: AbstractFloat} <: Interface{T}
+struct Glued <: Interface
 end
 
 """
@@ -276,11 +277,11 @@ end
 
 Build an optical interface from a AffineMap that transforms a point on the ellipsoid of the interface to the normal at that point, `normal`, and the refractive index ratio between the inside and the outside of the ellipsoid, `n`.
 """
-struct Active{T <: AbstractFloat} <: Interface{T}
-    normal::AffineMap{SDiagonal{3, T}, V3c{T}}
-    n::T
-    n²::T
-    Active(normal::AffineMap{SDiagonal{3,T},V3c{T}}, n::T) where {T <: AbstractFloat} = new{T}(normal, n, n^2)
+struct Active <: Interface
+    normal::AffineMap{SDiagonal{3, Float64}, V3}
+    n::Float64
+    n²::Float64
+    Active(normal::AffineMap{SDiagonal{3, Float64}, V3}, n::Float64) = new(normal, n, n^2)
 end
 
 refract(n, dir, a, b, N) = n*dir + (n*a - sqrt(1 - b))*N
@@ -292,7 +293,7 @@ reflect(dir, a, N) = dir + 2a*N
 
 Refract or reflect a ray with an interface. Update the direction of the ray. Returns if event failed, which is always `true` (see `raytrace!` for details why that is so).
 """
-function bend!(r::Ray{T}, i::Active{T}) where {T <: AbstractFloat}
+function bend!(r::AbstractRay, i::Active)
     N = normalize(i.normal(r.orig))
     a = -r.dir⋅N
     b = i.n²*(1 - a^2)
@@ -305,28 +306,28 @@ function bend!(r::Ray{T}, i::Active{T}) where {T <: AbstractFloat}
     nothing
 end
 
-bend!(r::Ray{T}, i::Glued{T}) where {T <: AbstractFloat} = nothing
+bend!(r::AbstractRay, i::Glued) = nothing
 
 """
     OpticUnit(surface::Ellipsoid, pointin::Bool, n::Float64, register::Bool, name::String)
 
 Build an optical unit from a surface. `pointin` dictates if the normal should be pointing in or out. `n` is the refractive index. `register` indicates whether this unit should register intersection points (thus functions as a retina).  
 """
-struct OpticUnit{T <: AbstractFloat}
-    surface::Ellipsoid{T}
-    medium::Medium{T}
-    interface::Interface{T}
+struct OpticUnit
+    surface::Ellipsoid
+    medium::Medium
+    interface::Interface
 
-    function OpticUnit(surface::Ellipsoid{T}, pointin::Bool, n::T, medium::Medium{T}) where {T <: AbstractFloat}
+    function OpticUnit(surface::Ellipsoid, pointin::Bool, n::Float64, medium::Medium)
         interface = if n ≠ 1
-            i = (-one(T))^pointin
+            i = Float64((-1)^pointin)
             dir = LinearMap(SDiagonal(i, i, i))
             normal = dir∘surface.scale∘surface.scale∘surface.center
             Active(normal, n)
         else
-            Glued{T}()
+            Glued()
         end
-        new{T}(surface, medium, interface)
+        new(surface, medium, interface)
     end
 end
 
@@ -335,13 +336,13 @@ end
 
 Advance a ray to the intersection point with an ellipsoid. If the intersection was successful, bend the ray. Updates the ray accordingly. Returns intersection failure.
 """
-function raytrace!(r::Ray{T}, c::OpticUnit{T}) where {T <: AbstractFloat}
+function raytrace!(r::AbstractRay, c::OpticUnit)
     l = distance(r, c.surface)
     absorbmove!(r, l, c.medium)
     bend!(r, c.interface)
 end
 
-function raytrace!(r::Ray{T}, ous::Vector{OpticUnit{T}}) where {T <: AbstractFloat}
+function raytrace!(r::AbstractRay, ous::Vector{OpticUnit})
     try
         foreach(ous) do ou
             raytrace!(r, ou)
@@ -353,74 +354,74 @@ end
 
 # light
 
-abstract type Light{T} end
+struct Light
+    cosa::Float64
+    orig::V3
+    rotm::LinearMap{RotY{Float64}}
+    nom::Float64
+    nodal::V3
+    radius::Float64
 
-struct Near{T <: AbstractFloat} <: Light{T}
-    cosa::T
-    orig::V3c{T}
-    rotm::LinearMap{RotY{T}}
-end
-
-struct Far{T <: AbstractFloat} <: Light{T}
-    R::T
-    z::T
-    dir::V3c{T}
-    rotm::AffineMap{RotY{T}, V3c{T}}
-end
-
-function Light(distance::T, aperture::T, rxy::T, rz::T, cz::T, θ::T) where {T <: AbstractFloat}
-    r = aperture/2
-    h = sqrt((1 - r^2/rxy^2)*rz^2) + cz
-    rotm = LinearMap(RotY(θ))
-    tran = recenter(rotm, V3c{T}(0,0,h))
-    if distance < 1e6 # todo, check accuracy here
-        orig = tran(V3c{T}(0,0,distance))
-        p2 = V3c{T}(r,0,h) - orig 
-        cosalpha1 = dot(normalize(-orig), normalize(p2))
-        p2 = V3c{T}(-r,0,h) - orig
-        cosalpha2 = dot(normalize(-orig), normalize(p2))
-        cosalpha3 = cos(atan(r/(distance - h)))
-        cosa = minimum([cosalpha1, cosalpha2, cosalpha3])
-        Near{T}(cosa, orig, rotm)
-    else
-        Far{T}(r, cz + rz + 100, rotm(V3c{T}(0,0,-1)), tran)
+    function Light(distance::Real, aperture::Real, rxy::Real, rz::Real, cz::Real, θ::Real)
+        radius = aperture/2
+        h = rz*sqrt(1 - (radius/rxy)^2)
+        nodal = V3(0, 0, cz + h)
+        rotm = LinearMap(RotY(θ))
+        tran = recenter(rotm, nodal)
+        orig = tran(nodal + V3(0, 0,distance))
+        v1 = nodal - orig
+        v2 = V3(radius, 0, h + cz) - orig
+        cosalpha1 = normalize(v1)⋅normalize(v2)
+        v2 = V3(-radius, 0, h + cz) - orig
+        cosalpha2 = normalize(v1)⋅normalize(v2)
+        cosalpha3 = cos(atan(radius/distance))
+        cosa = min(cosalpha1, cosalpha2, cosalpha3)
+        z = cz + h
+        nom = z - orig[3]
+        new(cosa, orig, rotm, nom, nodal, radius)
     end
+
 end
 
-function (l::Near{T})(b1::T, b2::T) where {T <: AbstractFloat}
+
+function (l::Light)(b1, b2; empty = false)
     z = b1*(l.cosa - 1) - l.cosa
     k = sqrt(1 - z^2)
     theta = 2b2
-    dir = l.rotm(V3c{T}(k*cospi(theta), k*sinpi(theta), z))
-    Ray(l.orig, dir)
+    dir = l.rotm(V3(k*cospi(theta), k*sinpi(theta), z))
+    t = l.nom/dir[3]
+    p0 = l.orig + t*dir
+    norm(p0 - l.nodal) > l.radius && throw(DeadRay())
+    empty ? EmptyRay(l.orig, dir) : Ray(l.orig, dir)
 end
 
-(l::Near{T})() where {T <: AbstractFloat} = l(rand(T), rand(T))
+(l::Light)(; empty = false) = l(rand(), rand(), empty = empty)
 
-function (l::Far{T})(b1::T, b2::T) where {T <: AbstractFloat}
-    k = l.R*sqrt(b1)
-    theta = 2b2
-    orig = l.rotm(V3c{T}(k*cospi(theta), k*sinpi(theta), l.z))
-    Ray(orig, l.dir)
+function (l::Light)(b; empty = true)
+    α = acos(l.cosa)
+    a = 2α*(b - 0.5)
+    dir = l.rotm(normalize(V3(sin(a), 0.0, -cos(a))))
+    t = l.nom/dir[3]
+    p0 = l.orig + t*dir
+    norm(p0 - l.nodal) > l.radius && throw(DeadRay())
+    empty ? EmptyRay(l.orig, dir) : Ray(l.orig, dir)
 end
 
-(l::Far{T})() where {T <: AbstractFloat} = l(rand(T), rand(T))
-
-#=struct ScallopEye{T <: AbstractFloat, L <: Light}
-    aperture::T
-    l::L
-    ous::Vector{OpticUnit{T}}
-    n::Int
-
-    function OpticalSystem(aperture::T, distance::T, θ::T, n::Int, tissues::Vector{Tissue}, membranes::Vector{Membrane}, morphz::T) where {T <: AbstractFloat}
-        new{T, L}(aperture, l, ous, n)
-    end
-end=#
-
-function raytrace!(l::Light{T}, ous::Vector{OpticUnit{T}}, n::Int) where {T <: AbstractFloat}
-    foreach(1:n) do _
-        r = l()
+function raytrace!(b, l::Light, ous::Vector{OpticUnit})
+    try
+        r = l(b)
         raytrace!(r, ous)
+    catch ex
+        ex isa DeadRay || throw(ex)
+    end
+end
+
+function raytrace!(b1, b2, l::Light, ous::Vector{OpticUnit})
+    try
+        r = l(b1, b2)
+        raytrace!(r, ous)
+    catch ex
+        ex isa DeadRay || throw(ex)
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,98 +2,112 @@ using Test
 using RayTraceEllipsoids, LinearAlgebra, CoordinateTransformations
 
 @test all(1:10^5) do _
-    oz, l, dz, hᵀ = rand(4) .- 0.5
-    a = RayTraceEllipsoids.isgoodz(oz, l, dz, hᵀ) 
+    oz, l, dz = rand(4) .- 0.5
+    positive = rand(Bool)
+    a = RayTraceEllipsoids.isgoodz(oz, l, dz, positive)
     z = oz + l*dz
-    b = hᵀ > 0 ? z ≥ hᵀ : z ≤ hᵀ
+    b = z > 0 ? positive : !positive
     a == b
 end
 
 
 @test all(1:10^5) do _
     z = rand()
-    orig = V3c(0.,0,z + 1)
-    dir = V3c(0.,0,-1)
-    hᵀ = 0.5
-    RayTraceEllipsoids.distance(orig, dir, hᵀ) == z
+    orig = V3(0.,0,z + 1)
+    dir = V3(0.,0,-1)
+    pos = true
+    RayTraceEllipsoids.distance(orig, dir, pos) == z
 end
 
-@test_throws RayTraceEllipsoids.DeadRay() RayTraceEllipsoids.distance(V3c(2,2,2.), V3c(0,0,-1.), 0.5)
+@test_throws RayTraceEllipsoids.DeadRay() RayTraceEllipsoids.distance(V3(2,2,2.), V3(0,0,-1.), true)
 
 @test all(1:10^5) do _
     z = rand()
-    c = V3c(1.,1,1)
-    r = V3c(1.,2,3)
-    h = .5
-    name = "a"
-    s = Ellipsoid(c, r, h, name)
-    orig = V3c(1,1,c[3] + r[3] + z)
-    dir = V3c(0.,0,-1)
+    c = V3(1.,1,1)
+    r = V3(1.,2,3)
+    h = true
+    s = Ellipsoid(c, r, h)
+    orig = V3(1,1,c[3] + r[3] + z)
+    dir = V3(0.,0,-1)
     r = Ray(orig, dir)
     RayTraceEllipsoids.distance(r, s) ≈ z
 end
 
+
 @test all(1:10^5) do _
-    orig = V3c(0.,0,0)
+    z = rand()
+    c = V3(1.0,1,1)
+    r = V3(1.0,2,3)
+    h = true
+    s = Ellipsoid(c, r, h)
+    orig = V3(1.0,1,c[3] + r[3] + z)
+    dir = V3(0.0,0,-1)
+    r = Ray(orig, dir)
+    RayTraceEllipsoids.distance(r, s) ≈ z
+end
+
+
+
+@test all(1:10^5) do _
+    orig = V3(0.,0,0)
     l = rand()
-    dir = V3c(0.,0,-1)
-    o = V3c(rand(3)...)
+    dir = V3(0.,0,-1)
+    o = V3(rand(3)...)
     ucs = Translation(o)
     rorig = o
     RayTraceEllipsoids._getl(orig, l, dir, ucs, rorig) == l
 end
 
 #1,3
-r = Ray(V3c(2.,2,2), V3c(0.,0,-1))
+r = Ray(V3(2.,2,2), V3(0.,0,-1))
 s = Cylinder(1.5,-2.,2.)
 L = 4.
 @test RayTraceEllipsoids.incylinder(r, s, L) == 0
 #2
-r = Ray(V3c(1.,1,2), V3c(0.,0,-1))
+r = Ray(V3(1.,1,2), V3(0.,0,-1))
 s = Cylinder(1.5,-2.,2.)
 L = 4.
 @test RayTraceEllipsoids.incylinder(r, s, L) == 4
 #4
-r = Ray(V3c(0.,0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+r = Ray(V3(0.,0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 s = Cylinder(1.,-2.,2.)
 L = sqrt(4^2+4^2)
 @test RayTraceEllipsoids.incylinder(r, s, L) == sqrt(2)
 #5
-r = Ray(V3c(1.,-1,2), V3c(0.,1,-1))
+r = Ray(V3(1.,-1,2), V3(0.,1,-1))
 s = Cylinder(1.,-2.,2.)
 L = sqrt(4^2+4^2)
 @test RayTraceEllipsoids.incylinder(r, s, L) == 0
 #6
-r = Ray(V3c(-4tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+r = Ray(V3(-4tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 s = Cylinder(1.,-2.,2.)
 L = sqrt(4^2+4^2)
 @test RayTraceEllipsoids.incylinder(r, s, L) ≈ sqrt(2)
 #7
-r = Ray(V3c(-2tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+r = Ray(V3(-2tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 s = Cylinder(1.,-2.,2.)
 L = sqrt(4^2+4^2)
 @test RayTraceEllipsoids.incylinder(r, s, L) ≈ sqrt(2^2+2^2)
 #8
-r = Ray(V3c(-6tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+r = Ray(V3(-6tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 s = Cylinder(1.,-2.,2.)
 L = sqrt(4^2+4^2)
 @test RayTraceEllipsoids.incylinder(r, s, L) == 0
 
 
 s = Cylinder(1.,-2.,2.)
-m = Retina(s, 0.1, "a")
-r = Ray(V3c(-2tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+m = Retina(s, 0.1)
+r = Ray(V3(-2tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 l = sqrt(4^2+4^2)
 L = sqrt(2^2+2^2)
 signal = (1 - RayTraceEllipsoids.absorption(L, 0.1))
 RayTraceEllipsoids.register!(r, l, m)
 @test m.signal[] == signal
-@test m.n[] == 1
 
 
 s = Cylinder(1.,-2.,2.)
-m = Retina(s, 0.1, "a")
-r = Ray(V3c(-2tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+m = Retina(s, 0.1)
+r = Ray(V3(-2tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 l = sqrt(4^2+4^2)
 o2 = r.orig + r.dir*l
 RayTraceEllipsoids.absorbmove!(r, l, m)
@@ -102,9 +116,30 @@ i = RayTraceEllipsoids.absorption(l, 0.1)
 @test r.orig == o2
 
 s = Cylinder(1.,-2.,2.)
-m = Retina(s, 1 - RayTraceEllipsoids.ENERGYTHRESHOLD, "a")
-r = Ray(V3c(-2tand(45),0,2), V3c(sqrt(1/2),0,-sqrt(1/2)))
+m = Retina(s, 1 - RayTraceEllipsoids.ENERGYTHRESHOLD)
+r = Ray(V3(-2tand(45),0,2), V3(sqrt(1/2),0,-sqrt(1/2)))
 l = sqrt(4^2+4^2)
 o2 = r.orig + r.dir*l
 @test_throws RayTraceEllipsoids.DeadRay() RayTraceEllipsoids.absorbmove!(r, l, m)
 
+@test all(1:10^5) do _
+    z = rand()
+    c = V3(1.0,1,1)
+    r = V3(2.0,2,2)
+    orig = V3(c[1] + 1, c[2] + 1, c[3] + r[3] + z)
+    dir = V3(0.0,0,-1)
+    p1 = copy(dir)
+    ray = Ray(orig, dir)
+    h = true
+    ss = Ellipsoid(c, r, h)
+    l = RayTraceEllipsoids.distance(ray, ss)
+    s = Cylinder(r[1], c[2] - r[2], c[3] + r[3])
+    m = Retina(s, 0.5)
+    RayTraceEllipsoids.absorbmove!(ray, l, m)
+    ou = OpticUnit(ss, false, 0.9, m)
+    RayTraceEllipsoids.bend!(ray, ou.interface)
+    p2 = ray.dir
+    acosd(dot(p1, p2) / sqrt(norm(p1)*norm(p2))) ≈ 45 - asind(sind(45)*0.9)
+end
+
+# l = Light(1m, 1mm, 1mm, 1mm, 1mm, 1rad)
